b0c80323697c1a8e758e979df3a8e00c0831d2e4
chess-seventh <chess7th@pm.me>
2019-02-16 01:45:57 +0100

    Add date parsing for `now`

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@
 // use serde_json::json;
 // use std::io::{self, Write};
 // use std::io::{BufRead, BufReader};
-use std::process::Command;
 // use std::process::{Command, Stdio};
+//use serde_json::{Result, Value};
+extern crate chrono;
+use chrono::prelude::*;
+use std::process::Command;
 use serde::{Deserialize, Serialize};
-use serde_json::{Result, Value};
+use serde_json;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Annotations {
@@ -16,45 +19,41 @@ struct Annotations {
 #[derive(Serialize, Deserialize, Debug)]
 struct Taskwarrior {
     id: u16,
-    description: String,
-    due: String,
-    priority: String,
+    description: Option<String>,
+    due: Option<String>,
+    priority: Option<String>,
     annotations: Option<Vec<Annotations>>,
     project: String,
-    tags: Vec<String>,
+    tags: Option<Vec<String>>,
     uuid: String,
     urgency: f32,
 }
 
 fn main() {
-    // run2()
-    let output = Command::new("task")
-        .arg("due")
-        .arg("project:homelab")
-        .arg("export")
-        .output()
-        .expect("failed to execute process");
+    let mjson = get_tasks();
+    // println!("{:?}", mjson);
 
-    if output.status.success() {
-        let out = String::from_utf8(output.stdout).unwrap().to_owned();
-        let json_out: Vec<Taskwarrior> = serde_json::from_str(&out).unwrap();
-        println!("json_out: {:?}", json_out);
+    for t in mjson {
+        let due_date = t.due.unwrap(); // .unwrap();
+        // println!("{:?}", DateTime::due_date.format("%Y%m%dT%H%M%S%Z"));
+        // let custom = DateTime::parse_from_str(&due_date, "%Y%m%dT%H%M%SZ");
+
+        println!("{:?}", due_date);
     }
+
 }
 
 fn get_tasks() -> Vec<Taskwarrior> {
-    // let mut pro
 
     let output = Command::new("task")
+        .arg("project:home")
         .arg("due")
-        .arg("project:homelab")
         .arg("export")
         .output()
         .expect("failed to execute process");
 
-    if output.status.success() {
-        let out = String::from_utf8(output.stdout).unwrap().to_owned();
-        let json_out: Vec<Taskwarrior> = serde_json::from_str(&out).unwrap();
-        println!("json_out: {:?}", json_out);
-    }
+    let out = String::from_utf8(output.stdout).unwrap().to_owned();
+    let json_out: Vec<Taskwarrior> = serde_json::from_str(&out).unwrap();
+
+    return json_out;
 }
