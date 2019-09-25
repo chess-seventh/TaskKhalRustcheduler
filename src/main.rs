632b322233468c0744ce3dfbb506111e3805e369
chess-seventh <chess7th@pm.me>
2019-09-25 07:38:37 +0200

    Add CLAP dependency for argument parsing
    Add Cargo lock dependencies
    Add Parsing the command line arguments
    Fix Naming of variables
    Fix renaming of variables

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
-// use serde::{Deserialize, Serialize};
-// use serde_json::json;
-// use std::io::{self, Write};
-// use std::io::{BufRead, BufReader};
-// use std::process::{Command, Stdio};
-//use serde_json::{Result, Value};
 extern crate chrono;
-// use chrono::prelude::*;
+extern crate clap;
 use std::process::Command;
+
 use serde::{Deserialize, Serialize};
 use serde_json;
 
-// use ics::properties::{Comment, Summary};
+use clap::{Arg, App}; // , SubCommand};
+
 use ics::properties::Summary;
 use ics::{ICalendar, ToDo};
 
@@ -34,38 +30,98 @@ struct Taskwarrior {
 }
 
 fn main() {
-    let mjson = get_tasks();
-    let mut calendar = ICalendar::new("2.0", "ics-rs");
+    let matches = App::new("task_khal")
+        .version("1.0")
+        .about("Your Taskwarrior tasks into your khal calendar!")
+        .author("Francesco Piva <franci@piva.online>")
+        .arg(Arg::with_name("project")
+             .short("p")
+             .long("project")
+             .value_name("PROJECT")
+             .help("Sets the project to extract form Taskwarrior.")
+             .takes_value(true))
+        .arg(Arg::with_name("due")
+             .short("d")
+             .long("due")
+             .value_name("DUE")
+             .help("Extract only values with due date")
+             .takes_value(true))
+        .get_matches();
 
+    let project = matches.value_of("project").unwrap_or("");
+    //println!("Value for project: {}", project);
 
-    for t in mjson {
-        let due_date: &'str = t.due.unwrap(); // .unwrap();
-        let desc: &'str = t.description.unwrap();
-        let tid: &'str = t.uuid;
+    let due = matches.value_of("due").unwrap_or("");
+    //println!("Value for due: {}", due);
 
-        let mut todo = ToDo::new(tid, due_date);
-        todo.push(Summary::new(desc));
-        // todo.push(Comment::new("Buy her the Imagine Dragons tickets."));
+    let tasks = get_tasks(project.to_string(), due.to_string());
 
-        // println!("{:?}", DateTime::due_date.format("%Y%m%dT%H%M%S%Z"));
-        // let custom = DateTime::parse_from_str(&due_date, "%Y%m%dT%H%M%SZ");
+    for task in tasks {
+        let due_date = task.due.unwrap(); // .unwrap();
+        let desc = task.description.unwrap();
+        let task_id = task.uuid;
 
-        println!("{:?} {:?} {:?}", due_date, desc.replace('\n', ""), tid);
+        // println!("{:?} {:?} {:?}", due_date, desc.replace('\n', ""), task_id);
 
-        calendar.add_todo(todo);
+        let (_res, fname) = create_ics(due_date, desc, task_id);
+        match _res {
+            Ok(_res) => println!("All good. {:?}", fname),
+            Err(_res) => panic!("Something went wrong."),
+        }
     }
+}
+
+fn create_filename(task_id: &String) -> String {
+    //
+    // Function that creates the ics filename.
+    //
+    let mut fname: String = task_id.to_owned();
+    fname.push_str(".ics");
+    return fname;
 
 }
 
-fn get_tasks() -> Vec<Taskwarrior> {
 
-    let output = Command::new("task")
-        .arg("project:home")
-        .arg("due")
-        .arg("+READY")
-        .arg("export")
-        .output()
-        .expect("failed to execute process");
+fn create_ics(due_date: String, desc: String, task_id: String) -> (std::io::Result<()>, String) {
+    //
+    // Function that creates the ics.
+    //
+    let filename = create_filename(&task_id);
+
+    let mut calendar = ICalendar::new("2.0", "ics-rs");
+    let mut todo = ToDo::new(task_id, due_date);
+
+    todo.push(Summary::new(desc));
+    calendar.add_todo(todo);
+    let _res = calendar.save_file(filename.clone());
+
+    match _res {
+        Ok(_res)  => return (Ok(()), filename),
+        Err(_res) => panic!("Couldn't create the ics ! exiting now."),
+    }
+
+
+}
+
+
+fn get_tasks(project: String, due: String) -> Vec<Taskwarrior> {
+
+    let mut def_project = String::from("project:");
+    let mut def_due = String::from("due:");
+
+    if project.is_empty() {
+        let output = Command::new("task")
+            .arg(def_due)
+            .arg("+READY")
+            .arg("export")
+            .output()
+            .expect("failed to execute process");
+    }
+
+
+    def_project.push_str(&project);
+    def_due.push_str(&due);
+
 
     let out = String::from_utf8(output.stdout).unwrap().to_owned();
     let json_out: Vec<Taskwarrior> = serde_json::from_str(&out).unwrap();
